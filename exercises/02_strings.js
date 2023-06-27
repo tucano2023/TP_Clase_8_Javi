d97f7d8314118ce6b4d42fd2bf65816f6aa19b17
tucano2023 <jgiosa@yahoo.com>
2023-06-27 09:34:19 -0300

    Se corrigen variables string

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -1,18 +1,18 @@
 /*Ejercicio 02 - Strings - revisado*/
 
 function ejercicio2a(){
-    let palabra3 = "Anastasias";
+    let palabra3 = "Agisislao";
     let resultado3 = palabra3.substring(0, 5);
     alert(resultado3);
 }
 
 function ejercicio2b(){
-    let palabra4 = "Bilcapujio";
+    let palabra4 = "Thinkpad";
     alert(palabra4.charAt(0).toUpperCase() + palabra4.slice(1));
 }
 
 function ejercicio2c(){
-    let palabra5 ="Bilca pujio";
+    let palabra5 ="Eleanor Rigbi";
     let resultado4 = palabra5.indexOf(" ");
     alert("El espacio en blanco se encuentra despues del caracter número: " + resultado4);
 }
--- exercises/05_for.js
+++ exercises/05_for.js
@@ -1,13 +1,13 @@
 /* revisado */
 function ejercicio5a(){
-    const palabras = ["perro", "gato", "lobo", "canario", "pollo"];
+    const palabras = ["Fase1", "Fase2", "Fase3", "Fae4", "Fase5"];
     for(var i=0; i<5 ;i++){
         alert("La variable número "+ i + " contiene el valor "+palabras[i]);
     }
 }
 
 function ejercicio5b(){
-    const palabras = ["perro", "gato", "lobo", "canario", "pollo"];
+    const palabras = ["Fase1", "Fase2", "Fase3", "Fae4", "Fase5"];
     for(const i of palabras){
         alert(i[0].toLocaleUpperCase()+i.substring(1));
     }
